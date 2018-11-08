fd7b8d70be220602d19c26fe136a537c665e0755
Mohammad Reza <d3c0der@hotmail.com>
2018-11-08 17:21:12 +0330

    Create call.bat

---
+++ call.bat
@@ -0,0 +1,6 @@
+call "mainscripts\ServicesScript.bat" > LogServices.txt
+call "mainscripts\PolicyScript.bat" > LogPolicy.txt
+call "mainscripts\RegScript.bat" > LogRegScript.txt
+call "mainscripts\Permission.bat" > LogPermission.txt
+call "mainscripts\ScriptPort.bat" > LogPort.txt
+call "mainscripts\EnableLogFirewall.bat" > LogEnableLog.txt
