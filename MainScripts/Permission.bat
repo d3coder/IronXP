d2f586715d3d5b7abd39d5a26063a8ef2198fdf2
Mohammad Reza <d3c0der@hotmail.com>
2018-11-08 18:17:53 +0330

    Create Permission.bat

---
+++ MainScripts/Permission.bat
@@ -0,0 +1,108 @@
+s %systemroot%\system32\at.exe /E /R Users
+cacls %systemroot%\system32\at.exe /E /R "Power Users"
+cacls %systemroot%\system32\at.exe /E /P Administrators:R
+cacls %systemroot%\system32\at.exe /E /P System:R
+cacls %systemroot%\system32\attrib.exe /E /R Users
+cacls %systemroot%\system32\attrib.exe /E /R "Power Users"
+cacls %systemroot%\system32\attrib.exe /E /P Administrators:R
+cacls %systemroot%\system32\attrib.exe /E /P System:R
+cacls %systemroot%\system32\cacls.exe /E /R Users
+cacls %systemroot%\system32\cacls.exe /E /R "Power Users"
+cacls %systemroot%\system32\cacls.exe /E /P Administrators:R
+cacls %systemroot%\system32\cacls.exe /E /P System:R
+cacls %systemroot%\system32\debug.exe /E /R Users
+cacls %systemroot%\system32\debug.exe /E /R "Power Users"
+cacls %systemroot%\system32\debug.exe /E /P Administrators:R
+cacls %systemroot%\system32\debug.exe /E /P System:R
+cacls %systemroot%\system32\drwatson.exe /E /R Users
+cacls %systemroot%\system32\drwatson.exe /E /R "Power Users"
+cacls %systemroot%\system32\drwatson.exe /E /P Administrators:R
+cacls %systemroot%\system32\drwatson.exe /E /P System:R
+
+cacls %systemroot%\system32\drwtsn32.exe /E /R Users
+cacls %systemroot%\system32\drwtsn32.exe /E /R "Power Users"
+cacls %systemroot%\system32\drwtsn32.exe /E /P Administrators:R
+cacls %systemroot%\system32\drwtsn32.exe /E /P System:R
+cacls %systemroot%\system32\edlin.exe /E /R Users
+cacls %systemroot%\system32\edlin.exe /E /R "Power Users"
+cacls %systemroot%\system32\edlin.exe /E /P Administrators:R
+cacls %systemroot%\system32\edlin.exe /E /P System:R
+cacls %systemroot%\system32\eventcreate.exe /E /R Users
+cacls %systemroot%\system32\eventcreate.exe /E /R "Power Users"
+cacls %systemroot%\system32\eventcreate.exe /E /P Administrators:R
+cacls %systemroot%\system32\eventcreate.exe /E /P System:R
+cacls %systemroot%\system32\eventtriggers.exe /E /R Users
+cacls %systemroot%\system32\eventtriggers.exe /E /R "Power Users"
+cacls %systemroot%\system32\eventtriggers.exe /E /P Administrators:R
+cacls %systemroot%\system32\eventtriggers.exe /E /P System:R
+cacls %systemroot%\system32\ftp.exe /E /R Users
+cacls %systemroot%\system32\ftp.exe /E /R "Power Users"
+cacls %systemroot%\system32\ftp.exe /E /P Administrators:R
+cacls %systemroot%\system32\ftp.exe /E /P System:R
+
+cacls %systemroot%\system32\net.exe /E /R Users
+cacls %systemroot%\system32\net.exe /E /R "Power Users"
+cacls %systemroot%\system32\net.exe /E /P Administrators:R
+cacls %systemroot%\system32\net.exe /E /P System:R
+cacls %systemroot%\system32\net1.exe /E /R Users
+cacls %systemroot%\system32\net1.exe /E /R "Power Users"
+cacls %systemroot%\system32\net1.exe /E /P Administrators:R
+cacls %systemroot%\system32\net1.exe /E /P System:R
+cacls %systemroot%\system32\netsh.exe /E /R Users
+cacls %systemroot%\system32\netsh.exe /E /R "Power Users"
+cacls %systemroot%\system32\netsh.exe /E /P Administrators:R
+cacls %systemroot%\system32\netsh.exe /E /P System:R
+cacls %systemroot%\system32\rcp.exe /E /R Users
+cacls %systemroot%\system32\rcp.exe /E /R "Power Users"
+cacls %systemroot%\system32\rcp.exe /E /P Administrators:R
+cacls %systemroot%\system32\rcp.exe /E /P System:R
+cacls %systemroot%\system32\reg.exe /E /R Users
+cacls %systemroot%\system32\reg.exe /E /R "Power Users"
+cacls %systemroot%\system32\reg.exe /E /P Administrators:R
+cacls %systemroot%\system32\reg.exe /E /P System:R
+
+cacls %systemroot%\system32\regedt32.exe /E /R Users
+cacls %systemroot%\system32\regedt32.exe /E /R "Power Users"
+cacls %systemroot%\system32\regedt32.exe /E /P Administrators:R
+cacls %systemroot%\system32\regedt32.exe /E /P System:R
+cacls %systemroot%\system32\regsvr32.exe /E /R Users
+cacls %systemroot%\system32\regsvr32.exe /E /R "Power Users"
+cacls %systemroot%\system32\regsvr32.exe /E /P Administrators:R
+cacls %systemroot%\system32\regsvr32.exe /E /P System:R
+cacls %systemroot%\system32\rexec.exe /E /R Users
+cacls %systemroot%\system32\rexec.exe /E /R "Power Users"
+cacls %systemroot%\system32\rexec.exe /E /P Administrators:R
+cacls %systemroot%\system32\rexec.exe /E /P System:R
+cacls %systemroot%\system32\rsh.exe /E /R Users
+cacls %systemroot%\system32\rsh.exe /E /R "Power Users"
+cacls %systemroot%\system32\rsh.exe /E /P Administrators:R
+cacls %systemroot%\system32\rsh.exe /E /P System:R
+cacls %systemroot%\system32\runas.exe /E /R Users
+cacls %systemroot%\system32\runas.exe /E /R "Power Users"
+cacls %systemroot%\system32\runas.exe /E /P Administrators:R
+cacls %systemroot%\system32\runas.exe /E /P System:R
+
+cacls %systemroot%\system32\sc.exe /E /R Users
+cacls %systemroot%\system32\sc.exe /E /R "Power Users"
+cacls %systemroot%\system32\sc.exe /E /P Administrators:R
+cacls %systemroot%\system32\sc.exe /E /P System:R
+cacls %systemroot%\system32\subst.exe /E /R Users
+cacls %systemroot%\system32\subst.exe /E /R "Power Users"
+cacls %systemroot%\system32\subst.exe /E /P Administrators:R
+cacls %systemroot%\system32\subst.exe /E /P System:R
+cacls %systemroot%\system32\telnet.exe /E /R Users
+cacls %systemroot%\system32\telnet.exe /E /R "Power Users"
+cacls %systemroot%\system32\telnet.exe /E /P Administrators:R
+cacls %systemroot%\system32\telnet.exe /E /P System:R
+cacls %systemroot%\system32\tftp.exe /E /R Users
+cacls %systemroot%\system32\tftp.exe /E /R "Power Users"
+cacls %systemroot%\system32\tftp.exe /E /P Administrators:R
+cacls %systemroot%\system32\tftp.exe /E /P System:R
+cacls %systemroot%\system32\tlntsvr.exe /E /R Users
+cacls %systemroot%\system32\tlntsvr.exe /E /R "Power Users"
+cacls %systemroot%\system32\tlntsvr.exe /E /P Administrators:R
+cacls %systemroot%\system32\tlntsvr.exe /E /P System:R
+cacls %systemroot%\regedit.exe /E /R Users
+cacls %systemroot%\regedit.exe /E /R "Power Users"
+cacls %systemroot%\regedit.exe /E /P Administrators:R
+cacls %systemroot%\regedit.exe /E /P System:R
